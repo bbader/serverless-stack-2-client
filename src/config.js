03654c4b1c4ca6b8c0d2f0efc40bf7a28f16b45b
Bob Bader <bob.bader@mckesson.com>
2018-12-11 08:40:05 -0500

    Adding Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,23 +1,5 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
-  },
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
-  },
-  cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
-  }
-};
-
-
 const dev = {
+  STRIPE_KEY: "pk_test_Yn4iAG7U8Annz0KqknSbh6Q5",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-serverlessdeploymentbucket-vmrwyf0c473y"
@@ -35,6 +17,7 @@ const dev = {
 };
 
 const prod = {
+  STRIPE_KEY: "pk_test_Yn4iAG7U8Annz0KqknSbh6Q5",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-serverlessdeploymentbucket-16ts2vob4t6pe"
